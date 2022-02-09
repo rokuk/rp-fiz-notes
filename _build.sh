0465854c9dbf29b39fc17c1034cc263f20a3b10d
Rok Kuk <kontakt@rokuk.org>
2022-02-09 12:48:40 +0100

    fix image paths

--- _build.sh
+++ _build.sh
@@ -10,3 +10,6 @@ set -ev
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::word_document2')"
+
+cp -R images docs
